9b66653d6d840c6e7cc726b8f3f88910404944f7
ydebessu <ydebessu@yahoo.com>
2019-02-04 00:25:28 -0500

    Adding dependencies.

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,12 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+    public int countWords(String words) {
+        String[] wordsList = StringUtils.split(words, " ");
+        return (wordsList == null) ? 0: wordsList.length;
+    }
     
     public Application() {
         System.out.println ("Inside Application");
